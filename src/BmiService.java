bbefd3969c416c6b603edcebba1633f266fa614d
Sloth <austere4@gmail.com>
2024-05-28 20:57:55 +0300

    brackets added

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(int weightKG, int heightCM) {
-        int result = weightKG / heightCM*heightCM;
+        int result = weightKG / (heightCM * heightCM);
         return result;
     }
 }
